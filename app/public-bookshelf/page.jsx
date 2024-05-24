58e16acdd2f03ddb3be3eea9684b54a2154a1477
Corky's Dev Shop <corky@corkysdevshop.com>
2024-05-24 09:18:02 -0600

    documentation updates

--- app/public-bookshelf/page.jsx
+++ app/public-bookshelf/page.jsx
@@ -1,9 +1,12 @@
 // app/public-bookshelf/page.jsx
 
-import React from "react";
-import { ReadPublicBookshelfInteractor } from "@interactors/booklists/ReadPublicBookshelfInteractor";
-import Link from "next/link";
-import styles from "./page.module.css";
+import React from 'react';
+import { ReadPublicBookshelfInteractor } from '@interactors/booklists/ReadPublicBookshelfInteractor';
+import Link from 'next/link';
+import styles from './page.module.css';
+import AccordionWrapper from '@/app/(components)/AccordionWrapper';
+import Accordion from '@/app/(components)/Accordion';
+import { whatIsPublicBookshelfContent } from '@/app/faqs/accordionContent';
 
 export default async function PublicBooklistsPage() {
   const readPublicBookshelfInteractor =
@@ -12,34 +15,41 @@ export default async function PublicBooklistsPage() {
 
   return (
     <div>
-      <h1 className="text-2xl mb-4">Public Bookshelf</h1>
+      <h1 className='text-2xl mb-4'>Public Bookshelf</h1>
+      <AccordionWrapper title='Show More Information'>
+        <Accordion
+          title='What is the public bookshelf?'
+          content={whatIsPublicBookshelfContent}
+          isOpenByDefault={true}
+        />
+      </AccordionWrapper>
       {publicBooklists.length === 0 ? (
         <p>No public booklists available.</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {publicBooklists.map(async (booklist) => {
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {publicBooklists.map(async booklist => {
             const booklistOwner = booklist.booklistOwnerId;
 
             return (
               <div
                 key={booklist._id}
-                className="bg-secondary p-4 rounded-lg shadow-md"
+                className='bg-secondary p-4 rounded-lg shadow-md'
               >
                 <Link href={`/booklist/${booklist._id}`}>
-                  <h2 className="text-xl text-yellow mb-2">{booklist.title}</h2>
+                  <h2 className='text-xl text-yellow mb-2'>{booklist.title}</h2>
                 </Link>
                 <div className={styles.descriptionContainer}>
                   <p className={styles.descriptionText}>
                     {booklist.description}
                   </p>
                 </div>
-                <p className="text-xs mb-2">Books: {booklist.bookIds.length}</p>
+                <p className='text-xs mb-2'>Books: {booklist.bookIds.length}</p>
                 {booklistOwner && (
-                  <p className="text-sm">
-                    Booklist creator:{" "}
+                  <p className='text-sm'>
+                    Booklist curator:{' '}
                     <Link
                       href={`/profile/${booklistOwner.publicProfileName}`}
-                      className="text-yellow hover:text-orange"
+                      className='text-yellow hover:text-orange'
                     >
                       {booklistOwner.publicProfileName}
                     </Link>
