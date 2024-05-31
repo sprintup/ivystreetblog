8de4155752b9f64a268068d9f0f823e2ef2ccc0b
Corky's Dev Shop <corky@corkysdevshop.com>
2024-05-31 10:38:48 -0600

    fix 6

--- app/(components)/Footer.jsx
+++ app/(components)/Footer.jsx
@@ -2,10 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { FaChevronDown } from 'react-icons/fa';
-import { buildTimestamp, formattedDateTime } from '../utils/buildTimestamp';
-
-const buildTimestamp = Date.now();
-const formattedDateTime = new Date(buildTimestamp).toLocaleString();
+import { buildTimestamp, formattedDateTime } from '@app/utils/buildtimestamp';
 
 const Footer = () => {
   const [isVisible, setIsVisible] = useState(true);
