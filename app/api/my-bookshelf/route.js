69b7cc346aec893a9a734957deb9aa5558bfe8e1
Corky's Dev Shop <corky@corkysdevshop.com>
2024-05-31 10:20:25 -0600

    trying something different

--- app/api/my-bookshelf/route.js
+++ app/api/my-bookshelf/route.js
@@ -1,37 +1,35 @@
-import { ReadMyBookShelfInteractor } from '@/interactors/booklists/private/ReadMyBookShelfInteractor';
 
-export async function POST(request) {
-    try {
-        const { userEmail } = await request.json();
-        console.log('Received userEmail:', userEmail);
 
-        if (!userEmail) {
-            return new Response(JSON.stringify({ error: 'User email is required' }), {
-                status: 400,
-            });
-        }
+// export async function POST(request) {
+//     try {
+//         const { userEmail } = await request.json();
+//         console.log('Received userEmail:', userEmail);
 
-        const readBooklistsInteractor = await ReadMyBookShelfInteractor.create();
-        const booklists = await readBooklistsInteractor.execute(userEmail);
+//         if (!userEmail) {
+//             return new Response(JSON.stringify({ error: 'User email is required' }), {
+//                 status: 400,
+//             });
+//         }
 
-        if (booklists) {
-            return new Response(JSON.stringify(booklists), {
-                status: 200,
-                headers: {
-                    'Cache-Control': 'no-cache',
-                },
-            });
-        } else {
-            console.error('No booklists found for the provided email:', userEmail);
-            return new Response(
-                JSON.stringify({ error: 'No booklists found for the provided email' }),
-                { status: 404 }
-            );
-        }
-    } catch (error) {
-        console.error('Error in API route:', error);
-        return new Response(JSON.stringify({ error: error.toString() }), {
-            status: 500,
-        });
-    }
-}
+
+//         if (booklists) {
+//             return new Response(JSON.stringify(booklists), {
+//                 status: 200,
+//                 headers: {
+//                     'Cache-Control': 'no-cache',
+//                 },
+//             });
+//         } else {
+//             console.error('No booklists found for the provided email:', userEmail);
+//             return new Response(
+//                 JSON.stringify({ error: 'No booklists found for the provided email' }),
+//                 { status: 404 }
+//             );
+//         }
+//     } catch (error) {
+//         console.error('Error in API route:', error);
+//         return new Response(JSON.stringify({ error: error.toString() }), {
+//             status: 500,
+//         });
+//     }
+// }
