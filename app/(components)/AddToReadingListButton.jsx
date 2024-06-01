ecd2022e422b88a11287fcfcef4e0813c48056f6
Corky's Dev Shop <corky@corkysdevshop.com>
2024-06-01 15:08:16 -0600

    fix for reading list

--- app/(components)/AddToReadingListButton.jsx
+++ app/(components)/AddToReadingListButton.jsx
@@ -10,7 +10,7 @@ export default function AddToReadingListButton({ book }) {
 
   const handleAddToReadingList = async () => {
     try {
-      const response = await fetch('/api/user/tracked', {
+      const response = await fetch('/api/reading-list', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
