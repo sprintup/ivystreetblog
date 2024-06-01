bd9bcba0d1fd949be39abcf7a3c4413e2178f0b2
Corky's Dev Shop <corky@corkysdevshop.com>
2024-06-01 16:19:09 -0600

    recommendations docs

--- app/my-bookshelf/booklistEdit/recommendations/[booklistId]/OfferedRecommendation.jsx
+++ app/my-bookshelf/booklistEdit/recommendations/[booklistId]/OfferedRecommendation.jsx
@@ -57,48 +57,56 @@ export default function OfferedRecommendation({ recommendations, booklistId }) {
       {recommendations.length === 0 ? (
         <p>No offered recommendations.</p>
       ) : (
-        <ul className='space-y-4'>
-          {recommendations.map(recommendation => (
-            <li
-              key={recommendation._id}
-              className='bg-secondary text-accent p-4 rounded-lg shadow-md'
-            >
-              <Link
-                href={`/book/${recommendation?.bookId?._id}`}
-                className='text-yellow hover:text-orange'
+        <>
+          <p className='mb-4'>
+            Accepting a recommenation will not automatically add it to your
+            booklist. It will simply move to the accepted section below. Then
+            you can add the book to your reading list and decide to put it in
+            your booklist.
+          </p>
+          <ul className='space-y-4'>
+            {recommendations.map(recommendation => (
+              <li
+                key={recommendation._id}
+                className='bg-secondary text-accent p-4 rounded-lg shadow-md'
               >
-                {recommendation?.bookId?.Name}
-              </Link>
-              <div className='flex flex-col md:flex-row md:justify-between items-start md:items-center mb-2'>
-                <span className='text-sm font-bold mb-2 md:mb-0'>
-                  Recommended by:{' '}
-                  {recommendation?.recommendedBy?.publicProfileName}
-                </span>
-              </div>
-              <div className='flex justify-between items-center mb-2'>
-                <span className='text-sm font-bold'>
-                  <p className='text-accent mb-2'>
-                    Reason: {recommendation?.recommendationReason}
-                  </p>
-                </span>
-              </div>
-              <div className='flex flex-col md:flex-row md:justify-end items-center space-y-2 md:space-y-0 md:space-x-2'>
-                <button
-                  onClick={() => handleAccept(recommendation?._id)}
-                  className='px-2 py-1 bg-green-500 text-white rounded hover:bg-green-600 w-full md:w-auto'
+                <Link
+                  href={`/book/${recommendation?.bookId?._id}`}
+                  className='text-yellow hover:text-orange'
                 >
-                  Accept
-                </button>
-                <button
-                  onClick={() => handleReject(recommendation?._id)}
-                  className='px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600 w-full md:w-auto'
-                >
-                  Reject
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+                  {recommendation?.bookId?.Name}
+                </Link>
+                <div className='flex flex-col md:flex-row md:justify-between items-start md:items-center mb-2'>
+                  <span className='text-sm font-bold mb-2 md:mb-0'>
+                    Recommended by:{' '}
+                    {recommendation?.recommendedBy?.publicProfileName}
+                  </span>
+                </div>
+                <div className='flex justify-between items-center mb-2'>
+                  <span className='text-sm font-bold'>
+                    <p className='text-accent mb-2'>
+                      Reason: {recommendation?.recommendationReason}
+                    </p>
+                  </span>
+                </div>
+                <div className='flex flex-col md:flex-row md:justify-end items-center space-y-2 md:space-y-0 md:space-x-2'>
+                  <button
+                    onClick={() => handleAccept(recommendation?._id)}
+                    className='px-2 py-1 bg-green-500 text-white rounded hover:bg-green-600 w-full md:w-auto'
+                  >
+                    Accept
+                  </button>
+                  <button
+                    onClick={() => handleReject(recommendation?._id)}
+                    className='px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600 w-full md:w-auto'
+                  >
+                    Reject
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </div>
   );
